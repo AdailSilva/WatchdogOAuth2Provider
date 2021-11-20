c3ec208b800c3b7690abe3ccd69f9fb2da4532d0
Adail Silva <adail@bdmg.mg.gov.br>
2021-11-20 14:46:41 -0300

    Initial commit

---
+++ src/main/java/com/adailsilva/watchdog/WatchdogOAuth2ProviderApplication.java
@@ -0,0 +1,13 @@
+package com.adailsilva.watchdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchdogOAuth2ProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchdogOAuth2ProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adailsilva/watchdog/WatchdogOAuth2ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adailsilva.watchdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchdogOAuth2ProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
